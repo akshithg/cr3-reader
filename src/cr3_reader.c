fc0362b945a1f51b70aeada519bef07b80915de9
ag <akshith.573@gmail.com>
2024-07-10 11:42:19 +0000

    one line outputs

--- src/cr3_reader.c
+++ src/cr3_reader.c
@@ -30,22 +30,24 @@ void read_cr3_on_cpu(void *info)
     // Use inline assembly to read the CR3 register
     asm volatile("mov %%cr3, %0" : "=r" (cr3));
 
-    printk(KERN_INFO "[cr3reader] CPU %d: CR3: %lx\n", cpu, cr3);
+    //printk(KERN_INFO "[cr3reader] CPU %d: CR3 value for current process: %lx\n", cpu, cr3);
 
-    if ((mm = current->mm) != NULL) {
+    mm = current->mm;
+    if (mm) {
         unsigned long pgd = (unsigned long)mm->pgd;
-        printk(KERN_INFO "[cr3reader] CPU %d: PGD: %lx\n", cpu, pgd);
+        //printk(KERN_INFO "[cr3reader] CPU %d: PGD (CR3 equivalent) for current process: %lx\n", cpu, pgd);
 
         // Get the executable file path
         if (mm->exe_file) {
             char buffer[256];
             struct path exe_path_struct = mm->exe_file->f_path;
             char *path_buffer = d_path(&exe_path_struct, buffer, sizeof(buffer));
-            printk(KERN_INFO "[cr3reader] CPU %d: Executable path: %s\n", cpu, path_buffer);
+            printk(KERN_INFO "[cr3reader] cpu: %d cr3: %lx exe: %s\n", cpu, cr3, path_buffer);
         }
     }
-
-    printk(KERN_INFO "[cr3reader] CPU %d: Process name: %s\n", cpu, current->comm);
+    else {
+        printk(KERN_INFO "[cr3reader] cpu: %d cr3: %lx", cpu, cr3);
+    }
 }
 
 void read_cr3_and_process_info(struct timer_list *t)
@@ -53,8 +55,8 @@ void read_cr3_and_process_info(struct timer_list *t)
     // Run the read_cr3_on_cpu function on all CPUs
     on_each_cpu(read_cr3_on_cpu, NULL, 1);
 
-    // Re-arm the timer to fire again in 5 seconds
-    mod_timer(&my_timer, jiffies + msecs_to_jiffies(5000));
+    // Re-arm the timer to fire again in 2 seconds
+    mod_timer(&my_timer, jiffies + msecs_to_jiffies(2000));
 }
 
 static int __init cr3_read_init(void)
